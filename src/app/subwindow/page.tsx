cd0711be563e7d545e09efb24960a07b9f1ebaae
Ryohei HAYASHI <pey@ryohei.info>
2025-04-03 17:15:40 +0900

    initial commitment

---
+++ src/app/subwindow/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from '@/components/button';
+import { Link } from '@/components/link';
+
+export default function Page() {
+    return (
+        <Link href="/">Back to home</Link>
+    )
+}
